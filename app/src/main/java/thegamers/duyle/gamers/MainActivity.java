71abf962882fb6c9a78b508a1b9845ecb31d272d
Duy Le <leanhduy1998@gmail.com>
2016-11-10 17:32:13 -0500

    fix crappy code

--- app/src/main/java/thegamers/duyle/gamers/MainActivity.java
+++ app/src/main/java/thegamers/duyle/gamers/MainActivity.java
@@ -13,11 +13,8 @@ import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
-import com.github.clans.fab.FloatingActionMenu;
 
 public class MainActivity extends AppCompatActivity {
-    FloatingActionMenu materialDesignFAM;
-    FloatingActionButton floatingActionButton1, floatingActionButton2, floatingActionButton3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,11 +23,11 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton addButton = (FloatingActionButton) findViewById(R.id.addButton);
+        FloatingActionButton addButton = (FloatingActionButton) findViewById(R.id.addNewHabitButton);
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, TakePicture.class));
+                startActivity(new Intent(MainActivity.this, addNewHabit.class));
             }
         });
 
@@ -51,29 +48,7 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-        materialDesignFAM = (FloatingActionMenu) findViewById(R.id.material_design_android_floating_action_menu);
-        floatingActionButton1 = (FloatingActionButton) findViewById(R.id.material_design_floating_action_menu_item1);
-        floatingActionButton2 = (FloatingActionButton) findViewById(R.id.material_design_floating_action_menu_item2);
-        floatingActionButton3 = (FloatingActionButton) findViewById(R.id.material_design_floating_action_menu_item3);
 
-        floatingActionButton1.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                //TODO something when floating action menu first item clicked
-
-            }
-        });
-        floatingActionButton2.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                //TODO something when floating action menu second item clicked
-
-            }
-        });
-        floatingActionButton3.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                //TODO something when floating action menu third item clicked
-
-            }
-        });
 
 
     }
--- app/src/main/java/thegamers/duyle/gamers/addNewHabit.java
+++ app/src/main/java/thegamers/duyle/gamers/addNewHabit.java
@@ -7,19 +7,24 @@ import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Spinner;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
  * Created by Duy Le on 11/5/2016.
  */
 
-public class TakePicture extends AppCompatActivity {
+public class addNewHabit extends AppCompatActivity {
     Button takePictureB;
     ImageView imageView;
+    Spinner typeOfLengthSpinner;
     static final int CAM_REQUEST=1;
     Button doneButton;
     private static String day="day";
@@ -28,8 +33,9 @@ public class TakePicture extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //done button
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.take_picture);
+        setContentView(R.layout.add_new_habit);
         takePictureB=(Button) findViewById(R.id.takePicturebutton);
 
         doneButton = (Button) findViewById(R.id.doneButton);
@@ -37,10 +43,10 @@ public class TakePicture extends AppCompatActivity {
         doneButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(TakePicture.this, MainActivity.class));
+                startActivity(new Intent(addNewHabit.this, MainActivity.class));
             }
         });
-
+        //taking picture
         takePictureB.setOnClickListener(new View.OnClickListener(){
 
             @Override
@@ -51,6 +57,17 @@ public class TakePicture extends AppCompatActivity {
                 startActivityForResult(camera_intent,CAM_REQUEST);
             }
         });
+        // spinner for day/month/year
+        typeOfLengthSpinner = (Spinner) findViewById(R.id.typeOfLengthSpinner);
+            //spinner drop down elements
+        List<String> typeOfLength = new ArrayList<String>();
+        typeOfLength.add("Day(s)");
+        typeOfLength.add("Month(s)");
+        typeOfLength.add("Year(s)");
+            //adapter for spinner
+        ArrayAdapter dataAdapter = new ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item,typeOfLength);
+        typeOfLengthSpinner.setAdapter(dataAdapter);
+
     }
     private File getFile(){
         File folder = new File("/storage/external_SD/camera_app");
